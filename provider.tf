ed0ef505c8c243cd6f613f9b305e7b008bcd8620
Piotr Ścibisz <pscibisz@pgs-soft.com>
2022-10-22 16:39:14 +0200

    update

--- provider.tf
+++ provider.tf
@@ -11,7 +11,7 @@ terraform {
 
 provider "aws" {
   region  = var.AWS_REGION
-  profile = var.PROFILE
+#  profile = var.PROFILE
 
   access_key = var.AWS_ACCESS_KEY_ID
   secret_key = var.AWS_SECRET_ACCESS_KEY
